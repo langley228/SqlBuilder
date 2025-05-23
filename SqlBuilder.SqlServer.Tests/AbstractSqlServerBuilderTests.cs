e02fc1f7d645f1d0a0b35f61342e2dd516f2daea
Langley <langley228@gmail.com>
2025-05-23 11:06:50 +0800

    Add SqlServer support with DELETE and UPDATE builders
    - Created SqlBuilder.SqlServer project with necessary dependencies.
    - Implemented SqlServerDeleteBuilder and SqlServerUpdateBuilder for generating SQL DELETE and UPDATE statements.
    - Added extension methods for DbContext to facilitate SQL statement building.
    - Developed unit tests for DELETE and UPDATE operations to ensure correct SQL generation.
    - Normalized SQL output for consistent testing results.
    - Updated solution file to include new SqlServer projects and tests.

--- SqlBuilder.SqlServer.Tests/AbstractSqlServerBuilderTests.cs
+++ SqlBuilder.SqlServer.Tests/AbstractSqlServerBuilderTests.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using SqlBuilder.SqlServer;
 
 namespace SqlBuilder.SqlServerSql.Tests
--- SqlBuilder.SqlServer.Tests/SqlServerExtensionsTests.cs
+++ SqlBuilder.SqlServer.Tests/SqlServerExtensionsTests.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using SqlBuilder.SqlServer;
 
 namespace SqlBuilder.SqlServerSql.Tests
